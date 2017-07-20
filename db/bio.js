afb5a62410c564bfb94991a5fed5d1686e22f71b
lopezfmr <lopezfmr@users.noreply.github.com>
2017-07-20 13:35:20 -0500

    StarterPage
    1st attempt for the site

---
+++ db/bio.js
@@ -0,0 +1,248 @@
+//BIOGRAPHIC INFORMATION
+var boutMe = {
+    "name" : "Miguel R. Lopez",
+    "role" : "Manufacturing Engineer",
+    "contacts" : {
+        "mobile": "+1 (832) 474-4625",
+        "email": "lopezfmr@ou.edu",
+        "linkedin": "miguel-r-lópez-b162a315",
+        "location" : "Houston, TX",
+        "web" : "http://www.miguelruben.com"
+        },
+    "welcomeMessage": "Strong leadership qualities and personal integrity combined with the ingenuity and thinking process of a lean engineer work together to serve as a model for co-workers who seek guidance and advice.",
+    "skills" : {
+        "languages": [
+            {
+                "lang" : "English",
+                "level" : 5
+            },
+            {
+                "lang" : "Spanish",
+                "level" : 5
+            }
+        ],
+        "indEng": ["Lean manufacturing", "Six sigma", "Kaizen", "5S", "Value Stream Mapping", "Process Flow", "Visual Management", "BPMS", "Statistical Process Control", "Root Cause Analysis", "Plant Distribution", "Map Layout", "Process Improvements"],
+        "programming": ["HTML", "CSS", "JavaScript", "VBA with expertise in Access and Excel apps", "PAC Control", "Arduino", "Ladder Logic", "SharePoint", "SQL language", "UCOS EWS", "Arduino IDE", "C++"],
+        "software":["Microsoft SharePoint", "Microsoft Office (Access, Word, Excel, Visio, PowerPoint, Publisher, Outlook)", "Opto 22–PAC Project", "SAP", "Oracle", "SQL Databases", "Autodesk AutoCAD", "OnShape", "Matlab", "Minitab", "Arena", "SAS", "Adobe Photoshop", "Illustrator", "SketchUp", "Corel Draw", "Web design"]
+    },
+    "bioPic" : "images/miguelPicture.png"
+};
+
+//WORK INFORMATION
+var veDone= {
+    "jobs": [
+        {
+            "employer": "TechnipFMC",
+            "url": "http://www.tecnipfmc.com",
+            "title": "Global Production Engineer",
+            "location": "Houston, TX",
+            "dates": "January 2017 - Present",
+            "description": ["Designed and implemented multiple databases for identification of part (label and hard stamp)", "through the use of barcodes systems and zero manual input."]
+        },
+        {
+            "employer": "FMCTechnologies",
+            "url": "http://www.fmcti.com",
+            "title": "Lead Manufacturing Engineer",
+            "location": "Houston, TX",
+            "dates": "October 2015 - December 2016",
+            "description": ["Lead the implementation of the Manufacturing Execution System (MES) for SDS, Western Region Quality, ", "SS Warehouse, MPS, SS High and Low Bay. Also, trained 5 groups of engineers to be administrators."]
+        },
+        {
+            "employer": "FMCTechnologies",
+            "url": "http://www.fmcti.com",
+            "title": "Manufacturing Engineer",
+            "location": "Houston, TX",
+            "dates": "October 2015 - December 2016",
+            "description": ["Lead the implementation of the Manufacturing Execution System (MES) for SDS, Western Region Quality, ", "SS Warehouse, MPS, SS High and Low Bay. Also, trained 5 groups of engineers to be administrators."]
+        },
+        {
+            "employer": "",
+            "url": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": [""]
+        },
+        {
+            "employer": "",
+            "url": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": [""]
+        },
+        {
+            "employer": "",
+            "url": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": [""]
+        },
+        {
+            "employer": "",
+            "url": "",
+            "title": "",
+            "location": "",
+            "dates": "",
+            "description": [""]
+        }
+    ],
+    "projects": [
+        {
+            "title": "EMMA",
+            "dates": "2013",
+            "description": "Ergonomic Motion Managment for Assembly",
+            "images": [
+                "images/miguelPicture.png",
+                "images/miguelPicture.png"
+            ]
+        },
+        {
+            "title": "The Magnetic Solution",
+            "dates": "2015",
+            "description": "Selection of magnetic handles for the NO TOUCH Policy",
+            "images": [
+                "images/miguelPicture.png",
+                "images/miguelPicture.png"
+                ]
+        }
+    ],
+    "awards":[
+        {
+            "title": "Raymond C. Tower Commemorative Medal",
+            "by": "FMC Technologies’ corporate award",
+            "recongnition":"recognition of safety",
+            "dates":"October, 2016",
+            "url":"www.technipFMC.com",
+            "images": [
+                "images/miguelPicture.png",
+                "images/miguelPicture.png"
+                ]
+        }
+    ],
+    "activities": [
+        {
+            "actType": "Instructor of Lean Manufacturing 101 in Spanish",
+            "for":"FMC University",
+            "location":"Buenos Aires, Argentina",
+            "dates":"February 2015",
+            "images": [ 
+                "images/miguelPicture.png",
+                "images/miguelPicture.png"
+                ]
+        },
+        {
+            "actType": "Instructor of Lean Manufacturing 101 in Spanish",
+            "for":"FMC University",
+            "location":"Veracruz, Mexico",
+            "dates":"March 2015",
+            "images": [
+                "images/miguelPicture.png",
+                "images/miguelPicture.png"
+                ]
+        },
+        {
+            "actType": "Instructor of Lean Manufacturing 101",
+            "for":"FMC University",
+            "location":"Houston, TX",
+            "dates":"Since 2014",
+            "images": [
+                "images/miguelPicture.png",
+                "images/miguelPicture.png"
+                ]
+        }
+    ]
+};
+
+//EDUCATION INFORMATION
+var veLearned = {
+    "schools": [
+        {
+            "name": "University of Oklahoma",
+            "location": "Norman, OK, USA",
+            "degree": "Master of Science",
+            "major":["Industrial Engoneering"],
+            "dates":2009,
+            "url": "http://www.ou.edu",
+            "diplomaImage": "images/cc.png"
+        },
+        {
+            "name": "Universidad de Carabobo",
+            "location": "Valencia, Carabobo, Venezuela",
+            "degree": "Bachelor of Science",
+            "major": ["Industrial Engineering"],
+            "dates": 2004,
+            "url":"http://www.uc.edu.ve/",
+            "diplomaImage": "images/cc.png"
+        }
+    ],
+    "professionalTraining": [
+        {
+            "title": "Welding of Stainless Steels",
+            "institution": "American Welding Society - Educational",
+            "dates":"April 2012",
+            "certImage": "images/cc.png"
+        },
+        {
+            "title": "Liquid Penetrant Testing SNT-TC-1A Level I and II",
+            "institution": "Applied Technical Services, Inc",
+            "dates":"December 2011",
+            "certImage": "images/cc.png"
+        },
+        {
+            "title": "VBA Programming: A Comprehensive Hands-On Introduction",
+            "institution": "Learning Tree International",
+            "dates":"April 2011",
+            "certImage": "images/cc.png"
+        }
+    ],
+    "onlineCourses":[
+        {
+            "title": "JavaScript for Beginners",
+            "school": "Udacity",
+            "dates": 2016,
+            "url": "http://www.udacity.com"
+        },
+        {
+            "title": "JavaScript Promices",
+            "school": "Udacity",
+            "dates": 2016,
+            "url": "http://www.udacity.com"
+        },
+        {
+            "title": "JavaScript Fundamentals",
+            "school": "Khan Accademy",
+            "dates": 2016,
+            "url": "http://www.KhanAcademy.com"
+        }
+    ],
+    "publications":[
+        {
+            "authors": "Chen Ling, Miguel Lopez, & Jing Xing",
+            "year": 2007,
+            "title": "Validating Information Complexity Questionnaires Using Travel Web Sites",
+            "editorial": "Proceedings of Human-Computer Interaction International Conference 2007",
+            "location": "Beijing, P.R. China",
+            "dates": "22-27 July, 2007",
+            "url": "http://www.tecnipfmc.com"
+        },
+        {
+            "authors": "Marielvy Bolívar, Miguel López, & Agustín Mejías",
+            "year": 2004,
+            "title": "Diseño de un Sistema de Gestión de la Calidad para Programas de Maestría de una Universidad Pública",
+            "editorial": "XI Cumbre de Alta Gerencia Internacional en Administración y Marketing",
+            "location": "Argentina",
+            "dates": "2-5 Nov. 2006",
+            "url": "",
+        }
+    ]
+};
+
+// HOBBIES
+var vePlayed = {
+    "fishing" : [],
+    "photography" : [],
+    "FlyCopters" : [],
+    "funFacts" : []
+}
---
+++ js/script.js
@@ -0,0 +1,220 @@
+
+//add role
+var HTMLheaderRole = '<div id="role" class="color-secondary-2-1">%data%</div><hr>';
+var formattedRole = HTMLheaderRole.replace("%data%", boutMe.role);
+$("#top").prepend(formattedRole);
+// add Name
+var HTMLheaderName = '<h1 id="name" class="color-secondary-1-4">%data%</h1>';
+var formattedName = HTMLheaderName.replace("%data%", boutMe.name);
+$("#top").prepend(formattedName);
+
+//Contact Info 
+var HTMLmobile = '<li class="flex-item"><span class="color-primary-4">mobile:</span><a href="tel:%anchor%" target="_top" class="color-primary-1">%data%</a></li>';
+var anchorMobile = HTMLmobile.replace("%anchor%", boutMe.contacts.mobile);
+var formattedMobile = anchorMobile.replace("%data%", boutMe.contacts.mobile);
+
+var HTMLemail = '<li class="flex-item"><span class="color-primary-4">email:</span><a href="mailto:%anchor%" target="_top" class="color-primary-1">%data%</a></li>';
+var anchorEmail = HTMLemail.replace("%anchor%", boutMe.contacts.email);
+var formattedEmail = anchorEmail.replace("%data%",boutMe.contacts.email);
+
+var HTMLlinkedin = '<li class="flex-item"><span class="color-primary-4">linkedIn:</span><a href="https://www.linkedin.com/in/%anchor%/ target="_blank" class="color-primary-1">%data%</a></li>';
+var anchorLinkedIn = HTMLlinkedin.replace("%anchor%", boutMe.contacts.linkedin);
+var formattedLinkedIn = anchorLinkedIn.replace("%data%", boutMe.contacts.linkedin);
+
+var HTMLlocation = '<li class="flex-item"><span class="color-primary-4">location:</span><span class="color-primary-1">%data%</span></li>';
+var formattedLocation = HTMLlocation.replace("%data%",boutMe.contacts.location);
+
+$("#topContactInfo").append(formattedMobile + formattedEmail + formattedLinkedIn + formattedLocation)
+$("#footerContactInfo").append(formattedMobile + formattedEmail + formattedLinkedIn + formattedLocation)
+
+//bio Pic, welcome message, and kills
+var HTMLbioPic = '<img src="%data%s" class="biopic">';
+var formattedBioPic = HTMLbioPic.replace("%data%", boutMe.bioPic);
+$("#header").append(formattedBioPic);
+
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",boutMe.welcomeMessage);
+$("#header").append(formattedWelcomeMsg);
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="color-primary-1">%data%</span></li>';
+if (boutMe.skills.length > 0 ){
+    $("#header").append(HTMLskillsStart);
+    for (var skill = 0; skill < boutMe.skills.length; skill++) {
+        var formattedSkill = HTMLskills.replace("%data%", boutMe.skills[skill]);
+        $("#skills").append(formattedSkill);
+    }
+};
+
+//WORK INFORMATION
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+function displayWork() {
+    for (var job = 0; job < work.jobs.length; job++) {
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployeeTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployeeTitle);
+        
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	    $(".work-entry:last").append(formattedLocation);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+};
+displayWork();
+
+//PROJECTS SECTION
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%">';
+
+projects.display = function(){
+  for (var project = 0; project < projects.projects.length; project++){
+      //console.log(projects.projects[project].title);
+      $("#projects").append(HTMLprojectStart);
+      var formattedProjectTitle = HTMLprojectTitle.replace("%data%", professional.projects[project].title);
+      $(".project-entry:last").append(formattedProjectTitle);
+      var formattedProjectDates = HTMLprojectDates.replace("%data%", professional.projects[project].dates);
+      $(".project-entry:last").append(formattedProjectDates);
+      var formattedProjectDescription = HTMLprojectDescription.replace("%data%", professional.projects[project].description);
+      $(".project-entry:last").append(formattedProjectDescription);
+      if (professional.projects[project].images.length > 0){
+          for (var image = 0; image < professional.projects[project].images.length; image++){
+              var formatteProjectImage = HTMLprojectImage.replace("%data%", professional.projects[project].images[image]);
+              $(".project-entry:last").append(formatteProjectImage);
+          }
+      }
+  }  
+};
+projects.display();
+
+//EDUCATION INFORMATION
+
+//education related
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+function displaySchools(){
+    for (var school=0; school < education.schools.length; school++){
+        $("#education").append(HTMLschoolStart);
+        
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+        
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        $(".education-entry:last").append(formattedSchoolDates);
+        
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        $(".education-entry:last").append(formattedSchoolLocation);
+        
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+        $(".education-entry:last").append(formattedSchoolMajor);
+    };
+};
+displaySchools();
+
+//online classes
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineStart = '<div class="online-entry"></div>'
+var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
+var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+function displayOnlineCourses(){
+    $("#education").append(HTMLonlineClasses);
+    for (var course=0; course < education.onlineCourses.length; course++){
+        $("#education").append(HTMLonlineStart);
+        
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+        $(".online-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+        
+        var formattedOnlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+        $(".online-entry:last").append(formattedOnlineDate);
+    };
+};
+displayOnlineCourses();
+
+/* Google Map for the website */
+var googleMap = '<div id="map"></div>';
+var map;
+function initializeMap() {
+    var locations;
+    var mapOptions = {
+        disableDefaultUI: true
+    };
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    function locationFinder() {
+        var locations = [];
+        locations.push(boutMe.contacts.location);
+        education.schools.forEach(function(school){
+            locations.push(school.location);
+        });
+        work.jobs.forEach(function(job){
+            locations.push(job.location);
+        });
+        return locations;
+    }
+    function createMapMarker(placeData) {
+        var lat = placeData.geometry.location.lat();  // latitude from the place service
+        var lon = placeData.geometry.location.lng();  // longitude from the place service
+        var name = placeData.formatted_address;   // name of the place from the place service
+        var bounds = window.mapBounds;            // current boundaries of the map window
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            title: name
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            content: name
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infoWindow.open(map,marker);
+        });
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        map.fitBounds(bounds);// fit the map to the new marker
+        map.setCenter(bounds.getCenter());// center the map
+    }
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          createMapMarker(results[0]);
+        }
+    }
+    function pinPoster(locations) {
+        var service = new google.maps.places.PlacesService(map);
+        locations.forEach(function(place){
+            var request = {
+                query: place
+            };
+            service.textSearch(request, callback);
+        });
+    }
+    window.mapBounds = new google.maps.LatLngBounds(); // Sets the boundaries of the map based on pin locations
+    locations = locationFinder();
+    pinPoster(locations); // creates pins on the map for each location
+}
+window.addEventListener('load', initializeMap);
+window.addEventListener('resize', function(e) {
+    map.fitBounds(mapBounds);
+});
+
+$("#mapDiv").append(googleMap);
